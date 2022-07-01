893659c0cfa55b731c49e14514b4d08abdc115b5
Olivier Chotin <olivier.chotin@ocia.fr>
2022-07-01 09:59:51 +0200

    Signed-off-by: Olivier Chotin <olivier.chotin@ocia.fr>

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def index():
+    return "Hello world ! 12"
 
 if __name__ == "__main__":
-    app.run(debug=True)
-	
-	
+    app.run()
